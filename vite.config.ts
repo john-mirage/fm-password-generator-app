c4df98fe4f582149060e503b15db7718eb0fa4a7
John Mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-20 13:19:06 +0200

    fix: fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/password-generator-app/",
   resolve: {
     alias: {
       "@components": resolve(__dirname, "src/components"),
